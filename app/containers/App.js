27c877b493f607056ebc3da00641f92db9dd1032
schmartmann <stefanhartmann@gmail.com>
2018-03-31 20:45:14 -0400

    successfully add react desktop package

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import { View, Text } from 'react-desktop/macOs';
 
 type Props = {
   children: React.Node
@@ -10,9 +11,14 @@ export default class App extends React.Component<Props> {
 
   render() {
     return (
-      <div>
+      <View
+        background="#F8F8FF"
+        padding="20px"
+        horizontalAlignment="center"
+        verticalAlignment="center"
+        width="100%">
         {this.props.children}
-      </div>
+      </View>
     );
   }
 }
