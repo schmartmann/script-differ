bc1ee096d941fd19e64be758b796732b57a8bf36
schmartmann <stefanhartmann@gmail.com>
2018-03-31 20:02:03 -0400

    remove more presets

--- app/utils/Helpers.js
+++
@@ -1,6 +0,0 @@
-
-// function mapDispatchToProps( dispatch ) {
-//   return bindActionCreators( requestGithubToken ,dispatch )
-// };
-//
-// export default connect( null, mapDispatchToProps )( authGithub );
